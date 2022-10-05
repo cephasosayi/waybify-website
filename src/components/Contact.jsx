25d0231b43aafb172f0309cfdddaaf3330f74b95
Cephas Osayi <cephasosayi@gmail.com>
2022-10-05 15:33:03 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,26 +11,26 @@ function Contact() {
     <Container className={`${styles.contact}`} id='contact'>
       <h2 className={`${styles.title} mt-4 me-auto`}>React us</h2 >
       <div className={`${styles.row}`}>
-        <div >
+        <div className={`${styles.iconCon}`}>
         <a href='https://wa.me/2348068257152'>
             <img src={whatsapp} alt='whatsapp' className={`${styles.social}`} />
             </a>
         </div>
 
-        <div >
+        <div className={`${styles.iconCon}`}>
         <a href='#'>
-          <img src={twitter} alt='twitter' className={`${styles.social}`} />
+          <img src={twitter} alt='twitter' className={`${styles.social} `} />
         </a>
         </div>
 
-        <div>
+        <div className={`${styles.iconCon}`}>
           <a href='#'>
             <img src={facebook} alt='facebook' className={`${styles.social}`} />
             </a>
         </div>
 
-        <div className='d-flex fw-bold'>
-          <div></div><a href='tel:08068257152'> CALL US</a>
+        <div className={`${styles.iconCon} text-center fw-bold `} >
+          <a href='tel:08068257152'>CALL US</a>
         
 
         </div>
