4aef7860f60f5519fe03611c682a102f729863df
cephasosayi <cephasosayi@gmail.com>
2023-05-31 23:46:16 +0100

    drivers section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,41 +1,25 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import styles from './styles/Contact.module.css'
-import facebook from '../assets/icons/fb.png'
-import whatsapp from '../assets/icons/whatsapp2.png'
-import twitter from '../assets/icons/twitter.png'
-import call from '../assets/icons/call.png'
 
 function Contact() {
   return (
-    <Container className={`${styles.contact}`} id='contact'>
-      <h2 className={`${styles.title} mt-4 me-auto`}>React us</h2 >
-      <div className={`${styles.row}`}>
-        <div>
-        <a href='https://wa.me/2348068257152'>
-            <img src={whatsapp} alt='whatsapp' className={`${styles.social}`} />
-            </a>
-        </div>
-
-        <div>
-        <a href='#'>
-          <img src={twitter} alt='twitter' className={`${styles.social}`} />
-        </a>
-        </div>
-
-        <div>
-          <a href='#'>
-            <img src={facebook} alt='facebook' className={`${styles.social}`} />
-            </a>
-        </div>
-
-        <div className='d-flex fw-bold text-black'>
-          <div></div><a href='tel:08068257152' className='text-black'> CLICK TO CALL </a>
-        
-
-        </div>
-      </div>
-      
+    <Container fluid className={`${styles.container}`} id='contact'>
+           <Container>
+            <Row>
+            <Col xs={12} sm={12} md={7} lg={7}>
+              <h2 className={`${styles.heading}`}>Drive for Waybify</h2>
+              <p className={`${styles.desc}`}>Do you driver out of state a lot?, 
+              you can earn extra income as you travel. Apply today and start earning with Waybify</p>
+            </Col>
+            <Col xs={12} sm={12} md={5} lg={5} className={`${styles.btnContainer}`}>
+              <Button variant='primary' 
+              className={`${styles.btn} `}>
+                Join the Team!
+                </Button>
+            </Col>
+            </Row>
+           </Container>
     </Container>
   )
 }
