97eb32985ec4aa561837351b12c55e472aedfd58
cephasosayi <cephasosayi@gmail.com>
2023-06-01 00:50:02 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,49 +2,54 @@ import {MapPin} from 'react-feather'
 import React from 'react'
 import { Col, Container, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
 import styles from './styles/Footer.module.css'
+import logo from '../assets/New/logo.png';
+import ig from '../assets/New/ig.png';
+import fb from '../assets/New/fb.png';
+import twitter from '../assets/New/twitter.png';
+import linkedin from '../assets/New/linkedin.png';
 
 function Footer() {
   return (
-      <Container fluid className={`${styles.footer}`}>
-          <Container  >
-              <Row lg={3} md={3} sm={1} xs={1} className={`${styles.footerC}`}>
-                  <Col sm={12} xs={12} lg={6} md={4}>
-                      <div className='me-auto'>
-                      <p className={`${styles.copy} me-auto`}>Copyright 2022</p>
-                          <h3 className={`${styles.Ftitle} me-auto`}>About</h3>
-                          <p className={`${styles.desc}`}>We are a modern logistics company, we let you
-                              focus on growing your business while we handle the packaging 
-                        and delivery of your products to your customer outside your reach.
-                          </p>
-                          <p className={`${styles.desc}`}><MapPin color='#fff ' size={20}/> Karu Site, Opposite EFCC Academy.
-                          </p>
-                      </div>
+      <Container className={`${styles.footer} `}>
+              <Row className={`${styles.footerC} w-100`}>
+                  <Col sm={12} xs={12} lg={6} md={6} className={`${styles.ColContainer} mb-2`}>
+                    <div> 
+                        <img src={logo} className={`${styles.logo}`}/>
+                    </div>
+                    <div className={`${styles.socialContainer}`}>
+                        <div className={`${styles.iconBox}`}>
+                            <img src={ig} className={`${styles.icon}`} />
+                        </div>
+
+                        <div className={`${styles.iconBox}`}>
+                            <img src={fb} className={`${styles.icon}`} />
+                        </div>
+
+                        <div className={`${styles.iconBox}`}>
+                            <img src={twitter} className={`${styles.icon}`} />
+                        </div>
+
+                        <div className={`${styles.iconBox}`}>
+                            <img src={linkedin} className={`${styles.icon}`} />
+                        </div>
+
+                    </div>
                   </Col>
-                  <Col lg={3} sm={12} xs={12} md={4}>
-                  <h3 className={`${styles.title}`}>Quick Links</h3>
-                  <ListGroup fluid className={`${styles.list}`}>
-                      <ListGroupItem className={`${styles.listItem}`} ><a href='#hiws' > How it works</a></ListGroupItem>
-                      <ListGroup.Item className={`${styles.listItem}`}> FAQ</ListGroup.Item>
-                      <ListGroup.Item className={`${styles.listItem}`}> <a href='https://medium.com/@waybify' >Blog</a></ListGroup.Item>
+                  <Container  className='border border-1 border-dark mb-2 w-100 border-bottom '><div></div> </Container>
+                  <Col lg={3} sm={12} xs={12} md={4} className={`${styles.ColContainer}`}>
+                  <div className={`${styles.linksContainer}`} > 
+                  <p className={`${styles.links}`}>Terms of service</p>
+                  <p className={`${styles.links}`}>FAQs</p>
+                       <p className={`${styles.links}`}>Support</p>
+                  <p className={`${styles.links}`}>Careers</p>
+                    </div>
+                    <div className={`${styles.socialContainer}`}>
+                       <p className={`${styles.links}`}> Copyright © 2023 Waybify. All rights reserved.</p>
 
-                  </ListGroup>
+                    </div>
+                  
                   </Col>
-
-                  <Col lg={3} sm={12} xs={12} md={4} className={`${styles.lastFcol}`} >
-                  <h3  className={`${styles.title}`}>React us</h3>
-                  <ListGroup className={`${styles.list}`} fluid>
-                          <ListGroupItem className={`${styles.listItem}`}>
-                              <a href='https://wa.me/2348068257152'>
-                         WhatsApp </a></ListGroupItem>
-                          <ListGroup.Item className={`${styles.listItem}`}>
-                          <a href='https://instagram.com/waybify' >Instagram</a></ListGroup.Item>
-                      <ListGroup.Item className={`${styles.listItem}`}><a href='https://twitter.com/waybify' > Twitter </a></ListGroup.Item>
-
-                  </ListGroup>
-                  </Col>
-
-              </Row>
-        </Container>
+             </Row>
     </Container>
   )
 }
