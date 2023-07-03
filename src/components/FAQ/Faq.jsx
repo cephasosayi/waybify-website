c8841ac14268831a26ea434d02986bc746632e54
cephasosayi <cephasosayi@gmail.com>
2023-07-03 22:04:49 +0100

    FAQs update

--- src/components/FAQ/Faq.jsx
+++ src/components/FAQ/Faq.jsx
@@ -18,7 +18,7 @@ export const Faq = () => {
            <Accordion className={` ${styles.acc} ms-auto me-auto`}>
 
 
-  <Accordion.Item eventKey="1" className={`${styles.accItem}  mb-3 mb-lg-0 mb-xl-0`}>
+  <Accordion.Item eventKey="1" className={`${styles.accItem} `}>
     <Accordion.Header className={`bg-dark text-dark ${styles.accTitle}`}>
     How does Waybify work?
     </Accordion.Header>
@@ -29,7 +29,7 @@ export const Faq = () => {
     </Accordion.Body>
 </Accordion.Item>
 
-  <Accordion.Item eventKey="2" className={`${styles.accItem} mt-1 mt-lg-0 mt-xl-0`}>
+  <Accordion.Item eventKey="2" className={`${styles.accItem} `}>
     <Accordion.Header className={` bg-dark text-dark ${styles.accTitle}`}>What areas do you operate in?</Accordion.Header>
     <Accordion.Body className={`${styles.accbody}`}>
     Currently, Waybify operates in <b> Abuja, Jos and Lafia </b> in Nigeria. We are continuously 
@@ -37,18 +37,18 @@ export const Faq = () => {
     </Accordion.Body>
   </Accordion.Item> 
 
-  <Accordion.Item eventKey="3" className={`${styles.accItem}`}>
-    <Accordion.Header className={`bg-dark text-dark ${styles.accTitle}`}>
+  <Accordion.Item eventKey="3" className={`${styles.accItem} mb-3 mb-md-0 mb-lg-0 mb-xl-0`}>
+    <Accordion.Header className={`bg-dark text-dark  ${styles.accTitle}`}>
     What types of products can I deliver through Waybify?</Accordion.Header>
-    <Accordion.Body className={`${styles.accbody}`}>
-    Waybify is designed to handle a wide range of products, including but not limited to fashion items, 
-    electronics, home goods, and personal care products. However, <b> there may be certain restrictions on prohibited 
+    <Accordion.Body className={`${styles.accbody} `}>
+    We handle a wide range of products, including but not limited to fashion items, 
+    gadgets, and cosmetic/health products. However, <b> there may be certain restrictions on prohibited 
     items such as perishable goods </b>or those that require specialized handling. 
     Please refer customer care for more details.
     </Accordion.Body>
   </Accordion.Item>  
 
-  <Accordion.Item eventKey="4" className={`${styles.accItem}`}>
+  <Accordion.Item eventKey="4" className={`${styles.accItem} mt-1 mt-lg-0 mt-xl-0 mt-md-0`}>
     <Accordion.Header className={`bg-dark text-dark ${styles.accTitle}`}>
     How do I schedule a pickup?</Accordion.Header>
     <Accordion.Body className={`${styles.accbody}`}>
